4ee50d03c787948bea3922fb897cd848ed4dca9b
Riko Nusa Pratama <rico.nusa.p@hotmail.com>
2021-08-22 23:13:36 +0700

    main

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,3 +37,15 @@ function scrollHeader(){
     if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
 }
 window.addEventListener('scroll', scrollHeader)
+
+
+
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/61160643d6e7610a49affb99/1fcv0gv0a';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
